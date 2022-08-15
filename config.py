ea872e05a8bdcb27dec79217f98e30e63f347c3a
Brian Semrau <brian@winterpixel.com>
2022-08-15 11:20:09 -0400

    generate docs in module folder, not godot main folder

--- config.py
+++ config.py
@@ -7,3 +7,30 @@ def configure(env):
 
 def get_icons_path():
     return "icons"
+
+def get_doc_classes():
+	return [
+		"Box2DShapeQueryParameters",
+		"Box2DWorld",
+		"Box2DCollisionObject",
+		"Box2DPhysicsTestMotionResult",
+		"Box2DPhysicsBody",
+		"Box2DKinematicCollision",
+		"Box2DArea",
+		"Box2DFixture",
+		"Box2DShape",
+		"Box2DCircleShape",
+		"Box2DRectShape",
+		"Box2DSegmentShape",
+		"Box2DPolygonShape",
+		"Box2DCapsuleShape",
+		"Box2DSDFShape",
+		"Box2DJoint",
+		"Box2DRevoluteJoint",
+		"Box2DPrismaticJoint",
+		"Box2DDistanceJoint",
+		"Box2DWeldJoint",
+	]
+
+def get_doc_path():
+	return "doc_classes"
