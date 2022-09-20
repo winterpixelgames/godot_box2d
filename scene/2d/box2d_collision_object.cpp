3adff8d2334f0b8635c4ca608496c0516cbf6a23
Jordan Schidlowsky <jordanschidlowsky@gmail.com>
2022-09-20 17:08:59 -0600

    remove jason crash

--- scene/2d/box2d_collision_object.cpp
+++ scene/2d/box2d_collision_object.cpp
@@ -413,8 +413,8 @@ int Box2DCollisionObject::get_colliding_contacts(Array p_array) const {
 				WARN_PRINT("[Box2DCollisionObject] get_colliding_contacts overflowed contacts buffer");
 				break;
 			}
-			Box2DContactImmutable* box2d_contact = Object::cast_to<Box2DContactImmutable>(p_array[i]);
-			box2d_contact->copy_contact(contact_iterator->contact);
+			Box2DContact* b2_contact = Object::cast_to<Box2DContact>(p_array[i]);
+			b2_contact->_contact = contact_iterator->contact;
 			i++;
 		}
 		contact_iterator = contact_iterator->next;
